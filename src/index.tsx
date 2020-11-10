7dc2c6b1f14868857a59df70703e0ae19b7d7013
Ivan Ivanov <ivan.kb@bk.ru>
2020-11-10 23:48:55 +0700

    fixed all empty files

---
+++ src/index.tsx
@@ -0,0 +1,5 @@
+import ReactDOM from 'react-dom';
+import React from 'react';
+
+const app: HTMLElement | null = document.querySelector('#app');
+ReactDOM.render(<div>hello world, Tyt TS</div>, app);
